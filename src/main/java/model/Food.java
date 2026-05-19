d6b74582ee10e726d042417a0c1e3c2f8bdab644
Eugene <evgeniyzorich@mail.ru>
2026-05-19 14:06:00 +0300

    init: init project

---
+++ src/main/java/model/Food.java
@@ -0,0 +1,31 @@
+package model;
+
+interface Discountable {
+    double getDiscount();
+}
+
+public abstract class Food implements Discountable {
+    protected int amount;
+    protected double price;
+    protected boolean isVegetarian;
+
+    protected Food(int amount, double price, boolean isVegetarian) {
+        this.amount = amount;
+        this.price = price;
+        this.isVegetarian = isVegetarian;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isVegetarian() {
+        return isVegetarian;
+    }
+}
+
+
---
+++ src/main/java/model/constants/Discount.java
@@ -0,0 +1,6 @@
+package model.constants;
+
+public final class Discount {
+    public static final double NO_DISCOUNT = 0;
+    public static final double RED_APPLE_DISCOUNT = 60;
+}
